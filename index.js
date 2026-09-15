4e2dc9e9fc5f11f85e042f1850c2666012866694
*odohugochukwu* <*odohugochukwu502@gmail.com>
2026-09-15 14:20:37 +0100

    fixed atlast string

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 //import "dotenv/config" // this is used to load environment variables from a .env file into process.env and is common js
 
-require("dotenv").config() // this is used to load environment variables from a .env file into process.env and is common js
+require("dotenv").config();
 
-const express = require("express") //this is the framework we are using to build our server and is common js
-const mongoose = require('mongoose'); // moongoose is a library that allows us to interact with our mongoDB database and is common js
-const userRoute = require("./routes/userRoutes.js")
-const productRoute = require("./routes/productRoutes.js")
-const compass_string = process.env.COMPASS_STRING
-// this is used to load environment variables from a .env file into process.env and is common js
-const atlast_string = process.env.ATLAST_STRING
+const express = require("express");
+const mongoose = require("mongoose");
+const dns = require("dns");
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-//const compass_string = "mongodb://localhost:27017/cohort8_db" // creating database name
+const userRoute = require("./routes/userRoutes.js");
+const productRoute = require("./routes/productRoutes.js");
 
-//const atlast_string = "mongodb+srv://odohugochukwu502_db_user:<ugochukwu502#>@cluster0.2tgvh4f.mongodb.net/cohort8_db?appName=Cluster0"// this my atlas string
+const compass_string = process.env.COMPASS_STRING;
+const atlast_string = process.env.ATLAS_STRING;
 
-mongoose.connect(atlast_string) // mongodb is used when we are working locally , while compass is online, connect our mongoos to our mongodb
-    .then(() => console.log("mongoDB connected ")) // help us hhandle a succcessfull message
-    .catch(err => console.error("connection Error: ", err));// handles error
+mongoose.connect(atlast_string)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
 
+const app = express();
 
-const app = express()
-const port = 5555
+const port = 5555;
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("server is active")
-})
-app.use("/users", userRoute) // comes after the app.use(express.json()) because we want to parse the body of the request before we send it to the route handler
-app.use("/products", productRoute) // comes after the app.use(express.json()) because we want to parse the body of the request before we send it to the route handler
+    res.send("server is active");
+});
+
+app.use("/users", userRoute);
+app.use("/products", productRoute);
+
 app.listen(port, () => {
-    console.log('server is up and running on port:{port}')
-})
+    console.log(`server is up and running on port:${port}`);
+});
 
 
 
